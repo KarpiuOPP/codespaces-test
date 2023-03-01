519b40426262cba1e2827d223c9c02df63a9f130
karpinskim <marcin.karpinski@vimncontractor.com>
2023-03-01 13:56:51 +0100

    First commit

---
+++ src/main/java/com/example/codespacestest/CodespacesTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.codespacestest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodespacesTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodespacesTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codespacestest/CodespacesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codespacestest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodespacesTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
